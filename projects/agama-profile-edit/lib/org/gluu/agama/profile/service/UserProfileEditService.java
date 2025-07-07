edfa938430be4f5e3214c642c13733dc846697f5
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-07-07 20:47:57 +0600

    feat: add UserProfileEditService.java

---
+++ projects/agama-profile-edit/lib/org/gluu/agama/profile/service/UserProfileEditService.java
@@ -0,0 +1,22 @@
+package org.gluu.agama.profile.service;
+
+import java.util.Map;
+
+import org.gluu.agama.profile.UserProfileEdit;
+
+public abstract class UserProfileEditService {
+
+    public abstract Map<String, String> getUserEntity(String mail);
+
+    public abstract String sendMail(String mail);
+
+    public abstract Map<String, Object> validateInputs(Map<String, String> profile);
+
+    public abstract String updateProfile (Map<String, String> profile);
+
+    public static UserProfileEditService getInstance(){
+        return UserProfileEdit.getInstance();
+    }
+
+}
+
