39a369360c7270d7fe34e6981e7fe712f635b720
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-12-01 20:28:12 +0600

    refactor: update UserProfileEditService.java

--- projects/agama-profile-edit/lib/org/gluu/agama/profile/service/UserProfileEditService.java
+++ projects/agama-profile-edit/lib/org/gluu/agama/profile/service/UserProfileEditService.java
@@ -2,21 +2,18 @@ package org.gluu.agama.profile.service;
 
 import java.util.Map;
 
-import org.gluu.agama.profile.UserProfileEdit;
+import org.gluu.agama.profile.UserProfileEditServiceImpl;
 
 public abstract class UserProfileEditService {
 
-    public abstract Map<String, String> getUserEntity(String mail);
-
-    public abstract String sendMail(String mail);
+    public abstract Map<String, String> getUserEntity(String userId);
 
     public abstract Map<String, Object> validateInputs(Map<String, String> profile);
 
-    public abstract String updateProfile (Map<String, String> profile, String mail);
+    public abstract String updateProfile (Map<String, String> profile, String userId);
 
     public static UserProfileEditService getInstance(){
-        return UserProfileEdit.getInstance();
+        return UserProfileEditServiceImpl.getInstance();
     }
 
 }
-
